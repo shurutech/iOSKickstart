a0f0db811f803e015726e66214dcec036533b913
Vijay <vijay@shuru.com>
2024-01-03 12:06:20 +0530

    Initial Commit

---
+++ SwiftAppTemplate/SwiftAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppTemplateApp.swift
+//  SwiftAppTemplate
+//
+//  Created by Vijay Goswami on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
