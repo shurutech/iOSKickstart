ea4784b3ee04b948640e5d2e8304cd3068799b9f
Vijay <vijay@shurutech.com>
2024-01-09 16:07:37 +0530

    add loader view

---
+++ SwiftAppTemplate/ReusableViews/LoaderView.swift
@@ -0,0 +1,46 @@
+//
+//  LoaderView.swift
+//  SwiftAppTemplate
+//
+//  Created by Vijay Goswami on 09/01/24.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var isLoading = true
+    
+    var body: some View {
+        if(isLoading) {
+            VStack {
+                ProgressView()
+            }
+            .padding(40)
+            .background(Color.white)
+            .cornerRadius(10)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.gray.opacity(0.5)
+                .edgesIgnoringSafeArea(.all))
+        }
+    }
+}
+
+struct LoaderViewModifier: ViewModifier {
+    var isLoading: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(LoaderView(isLoading: isLoading))
+    }
+}
+
+extension View {
+    func loader(_ value : Bool) -> some View {
+        modifier(LoaderViewModifier(isLoading: value))
+    }
+}
+
+#Preview {
+    Rectangle()
+        .loader(true)
+}
