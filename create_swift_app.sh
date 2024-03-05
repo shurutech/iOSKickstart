7645ff0bf77e2df4627c97172fec0e24768c0800
vijay-shuru <127201055+vijay-shuru@users.noreply.github.com>
2024-03-05 12:53:47 +0530

    Update create_swift_app.sh with Release tag variable

--- create_swift_app.sh
+++ create_swift_app.sh
@@ -31,12 +31,14 @@ fi
 
 OLD_APP_NAME="SwiftAppTemplate"
 DESTINATION_PATH="$HOME/Desktop/$NEW_APP_NAME"
+# Release tag to clone
+RELEASE_TAG="v0.0.1"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$DESTINATION_PATH"
 
-# Clone the template project into the destination directory
-git clone https://github.com/shurutech/iOSKickstart/tree/v0.0.1.git "$DESTINATION_PATH"
+# Clone the template project at a specific release tag into the destination directory
+git clone --branch "$RELEASE_TAG" --depth 1 https://github.com/shurutech/iOSKickstart.git "$DESTINATION_PATH"
 
 # Navigate to the cloned directory
 cd "$DESTINATION_PATH" || exit
